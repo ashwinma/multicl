e0c33d8cc56a619050bc967ad41c549ac297528b
bbowden1 <bbowden1>
2012-04-16 02:51:56 +0000

    git-svn-id: svn+ssh://charon.cs.vt.edu/repos/svn/gpu_perfmod/trunk@17 224d7eb1-a108-459d-91f0-3ca6aa5ee01a

---
+++ bowden/build.sh
@@ -0,0 +1,7 @@
+nvcc -O3 -Xopencc="-LIST:source=on -O3" -arch=sm_21 -o gpuThrputBenchmark gpuThrputBenchmark.cu
+
+
+nvcc -O0 -Xopencc="-LIST:source=on -O0" -arch=sm_21 -o gpuThrputBenchmark gpuThrputBenchmark.cu
+
+
+nvcc -O0 -Xopencc="-LIST:source=on -O0" -arch=sm_20 -o gpuThrputBenchmark gpuThrputBenchmark.cu

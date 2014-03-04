0bc3a94143da853dc16179c3434d213c7804825c
Ashwin Aji <ashwinma@gmail.com>
2014-03-04 20:46:57 +0000

    removing temp cuda files
    git-svn-id: svn+ssh://charon.cs.vt.edu/repos/svn/gpu_perfmod/branches/aaji@43 224d7eb1-a108-459d-91f0-3ca6aa5ee01a

--- snucl/src/runtime/hwloc/build-cuda/configure.sh
+++
@@ -1,1 +0,0 @@
-../configure --prefix=/home/aaji/opt --enable-cuda CPPFLAGS=-I/opt/apps/cuda/5.0.35/include LDFLAGS=-L/opt/apps/cuda/5.0.35/lib64
--- snucl/src/runtime/hwloc/build-cuda/tests/ports/topology-solaris-chiptype.c
+++
@@ -1,1 +0,0 @@
-../../../src/topology-solaris-chiptype.c

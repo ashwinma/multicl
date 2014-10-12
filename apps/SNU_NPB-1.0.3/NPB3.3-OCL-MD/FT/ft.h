6bf1bf9aba15e9ef022ee39cc5b0eeb482d0d5df
Ashwin Aji <ashwinma@gmail.com>
2014-10-12 06:22:02 +0000

    cl set work group info for FT
    git-svn-id: svn+ssh://charon.cs.vt.edu/repos/svn/gpu_perfmod/branches/aaji@89 224d7eb1-a108-459d-91f0-3ca6aa5ee01a
    
    Former-commit-id: 30caa7eadc602cd68f3d48c91e425085891aa5c1

--- apps/SNU_NPB-1.0.3/NPB3.3-OCL-MD/FT/ft.h
+++ apps/SNU_NPB-1.0.3/NPB3.3-OCL-MD/FT/ft.h
@@ -134,13 +134,15 @@
 #define ALPHA           1.0e-6
 
 
+#if 1
 #ifdef USE_CPU
-//#pragma OPENCL EXTENSION cl_amd_fp64: enable
-//#pragma OPENCL EXTENSION cl_amd_printf: enable
+#pragma OPENCL EXTENSION cl_amd_fp64: enable
+#pragma OPENCL EXTENSION cl_amd_printf: enable
 #else
-//#pragma OPENCL EXTENSION cl_khr_fp64: enable
-#endif
 #pragma OPENCL EXTENSION cl_khr_fp64: enable
+#endif
+#endif
+//#pragma OPENCL EXTENSION cl_khr_fp64: enable
 
 
 //---------------------------------------------------------------------------

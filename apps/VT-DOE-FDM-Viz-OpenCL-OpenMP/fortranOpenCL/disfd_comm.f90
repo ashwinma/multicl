cd5a4996031d530df3274121b8b051061a2a1411
Ashwin Aji <ashwinma@gmail.com>
2014-12-10 23:45:04 +0000

    huge change -- added gpu marshaling kernels and MPI-ACC hooks to this OpenCL version....need to debug seg faults though
    git-svn-id: svn+ssh://charon.cs.vt.edu/repos/svn/gpu_perfmod/branches/aaji@102 224d7eb1-a108-459d-91f0-3ca6aa5ee01a
    
    Former-commit-id: 1051b3639aabde8677a65cd008605686bc73a456
    Former-commit-id: af1f05e248df1f0c354e86421f2cceaad8c395be

--- apps/VT-DOE-FDM-Viz-OpenCL-OpenMP/fortranOpenCL/disfd_comm.f90
+++ apps/VT-DOE-FDM-Viz-OpenCL-OpenMP/fortranOpenCL/disfd_comm.f90
@@ -39,7 +39,7 @@ MODULE plane_wave_bd
 END MODULE plane_wave_bd 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 MODULE itface_comm
- use, intrinsic :: iso_c_binding, ONLY: C_FLOAT, C_PTR
+ use, intrinsic :: iso_c_binding
  implicit NONE
  save
  integer:: nyvx,nyvx1,nxvy,nxvy1,order_imsr,pid_ims,pid_imr
@@ -51,8 +51,30 @@ MODULE itface_comm
 ! send and receive calls), so that may be the reason.
 !
 ! Have to leave these as Fortran allocatable for now
-!
-real, allocatable, dimension(:):: sdy1,sdy2,sdx1,sdx2,rcy1,rcy2,rcx1,rcx2
+
+! ALIGNMENT ISSUES IN directly binding to C pointers
+!type(c_ptr),target :: c_sdx1D, c_sdy1D, c_sdx2D, c_sdy2D, c_rcx1D, c_rcy1D, c_rcx2D, c_rcy2D
+
+! bind(C, name='sdx1D') :: c_sdx1D
+! bind(C, name="sdy1D") :: c_sdy1D
+! bind(C, name="sdx2D") :: c_sdx2D
+! bind(C, name="sdy2D") :: c_sdy2D
+! bind(C, name="rcx1D") :: c_rcx1D
+! bind(C, name="rcy1D") :: c_rcy1D
+! bind(C, name="rcx2D") :: c_rcx2D
+! bind(C, name="rcy2D") :: c_rcy2D
+
+! Workaround
+integer(c_int) :: c_sdx1_id = 10, &
+                c_sdy1_id = 11 , &
+                c_sdx2_id = 12 , &
+                c_sdy2_id = 13 , &
+                c_rcx1_id = 14 , &
+                c_rcy1_id = 15 , &
+                c_rcx2_id = 16 , &
+                c_rcy2_id = 17 
+
+real(c_float), allocatable, target, dimension(:):: sdy1,sdy2,sdx1,sdx2,rcy1,rcy2,rcx1,rcx2
 ! real(c_float), pointer, dimension(:):: sdy1,sdy2,sdx1,sdx2,rcy1,rcy2,rcx1,rcx2
 ! type (c_ptr), target :: cptr_sdy1,cptr_sdy2,cptr_sdx1,cptr_sdx2,cptr_rcy1, &
 !      cptr_rcy2,cptr_rcx1,cptr_rcx2
@@ -62,7 +84,7 @@ real, allocatable, dimension(:):: sdy1,sdy2,sdx1,sdx2,rcy1,rcy2,rcx1,rcx2
 !
 ! Have to leave these as Fortran allocatable for now
 !
- real, allocatable, dimension(:,:):: chx,chy,cix,ciy
+ real(c_float), allocatable, target, dimension(:,:):: chx,chy,cix,ciy
 ! real(c_float), pointer, dimension(:,:):: chx,chy,cix,ciy
 ! real(c_float), pointer, dimension(:,:):: chx_init,chy_init,cix_init,ciy_init
 ! type (c_ptr), target :: cptr_chx,cptr_chy,cptr_cix,cptr_ciy
@@ -300,8 +322,60 @@ MODULE wave_field_comm
 
 ! waves in Region I
 ! real, allocatable, dimension(:,:,:):: v1x,v1y,v1z
+!
+
+
+!NOT DIRECTLY BINDING TO C POINTERS DUES TO ALIGNMENT ISSUES
+ !type(c_ptr), target :: c_v1xD, c_v1yD, c_v1zD, c_v2xD, c_v2yD, c_v2zD
+ !type(c_ptr), target :: c_t1xxD, c_t1xyD, c_t1xzD, c_t1yyD, c_t1yzD, c_t1zzD
+ !type(c_ptr), target :: c_t2xxD, c_t2xyD, c_t2xzD, c_t2yyD, c_t2yzD, c_t2zzD
+! bind(C, name='v1xD')  :: c_v1xD
+! bind(C, name='v1yD')  :: c_v1yD
+! bind(C, name='v1zD')  :: c_v1zD
+! bind(C, name='v2xD')  :: c_v2xD
+! bind(C, name='v2yD')  :: c_v2yD
+! bind(C, name='v2zD')  :: c_v2zD
+! bind(C, name='t1xxD') :: c_t1xxD
+! bind(C, name='t1xyD') :: c_t1xyD
+! bind(C, name='t1xzD') :: c_t1xzD
+! bind(C, name='t1yyD') :: c_t1yyD
+! bind(C, name='t1yzD') :: c_t1yzD
+! bind(C, name='t1zzD') :: c_t1zzD
+! bind(C, name='t2xxD') :: c_t2xxD
+! bind(C, name='t2xyD') :: c_t2xyD
+! bind(C, name='t2xzD') :: c_t2xzD
+! bind(C, name='t2yyD') :: c_t2yyD
+! bind(C, name='t2yzD') :: c_t2yzD
+! bind(C, name='t2zzD') :: c_t2zzD
+
+! RATHER A WORKAROUND
+
+integer(c_int) :: c_v1x_id = 40, &
+         c_v1y_id = 41, & 
+         c_v1z_id = 42, &
+         c_v2x_id = 43, &
+         c_v2y_id = 44, &
+         c_v2z_id = 45, &
+        c_t1xx_id = 46, &
+        c_t1xy_id = 47, &
+        c_t1xz_id = 48, &
+        c_t1yy_id = 49, &
+        c_t1yz_id = 50, &
+        c_t1zz_id = 51, &
+        c_t2xx_id = 52, &
+        c_t2xy_id = 53, &
+        c_t2xz_id = 54, &
+        c_t2yy_id = 55, &
+        c_t2yz_id = 56, &
+        c_t2zz_id = 57
+
  real(c_float), pointer,  dimension(:,:,:) :: v1x,v1y,v1z, v1x_init,v1y_init,v1z_init
  type (c_ptr), target :: cptr_v1x,cptr_v1y,cptr_v1z
+
+ real(c_float), pointer,  dimension(:,:,:) :: v1x_buff, v1y_buff, v1z_buff, &
+                                                v1x_init_buff, v1y_init_buff, v1z_init_buff
+ type (c_ptr), target :: cptr_v1x_buff, cptr_v1y_buff, cptr_v1z_buff
+
 ! real, allocatable, dimension(:,:,:):: t1xx,t1xy,t1xz,t1yy,t1yz,t1zz
  real(c_float), pointer,  dimension(:,:,:) :: t1xx,t1xy,t1xz,t1yy,t1yz,t1zz
  real(c_float), pointer,  dimension(:,:,:) :: t1xx_init,t1xy_init,t1xz_init, &
@@ -316,6 +390,10 @@ MODULE wave_field_comm
 ! real, allocatable, dimension(:,:,:):: v2x,v2y,v2z
  real(c_float), pointer, dimension(:,:,:):: v2x,v2y,v2z,v2x_init,v2y_init,v2z_init
  type (c_ptr), target :: cptr_v2x,cptr_v2y,cptr_v2z
+ 
+ real(c_float), pointer,  dimension(:,:,:) :: v2x_buff, v2y_buff, v2z_buff, &
+                                                 v2x_init_buff, v2y_init_buff, v2z_init_buff
+ type (c_ptr), target :: cptr_v2x_buff, cptr_v2y_buff, cptr_v2z_buff
 
 ! real, allocatable, dimension(:,:,:):: t2xx,t2xy,t2xz,t2yy,t2yz,t2zz
  real(c_float), pointer, dimension(:,:,:):: t2xx,t2xy,t2xz,t2yy,t2yz,t2zz
@@ -330,12 +408,56 @@ MODULE wave_field_comm
 ! real, allocatable, dimension(:,:,:):: sdx51,sdx41,sdy51,sdy41,rcx51,rcx41, &
 !                                       rcy51,rcy41,sdx52,sdx42,sdy52,sdy42, &
 !                                       rcx52,rcx42,rcy52,rcy42
- real(c_float), pointer, dimension(:,:,:):: sdx51,sdx41,sdy51,sdy41,rcx51,rcx41, &
-                                       rcy51,rcy41,sdx52,sdx42,sdy52,sdy42, &
-                                       rcx52,rcx42,rcy52,rcy42
+ real(c_float), pointer, dimension(:,:,:)::sdx51,sdx41,sdy51,sdy41,rcx51,rcx41, &
+                                       rcy51,rcy41,sdx52,sdx42,sdy52, &
+                                       sdy42,rcx52,rcx42,rcy52,rcy42
+
  type (c_ptr), target :: cptr_sdx51,cptr_sdx41,cptr_sdy51,cptr_sdy41,cptr_rcx51,cptr_rcx41, &
                                        cptr_rcy51,cptr_rcy41,cptr_sdx52,cptr_sdx42,cptr_sdy52, &
                                        cptr_sdy42,cptr_rcx52,cptr_rcx42,cptr_rcy52,cptr_rcy42
+
+! NOT binding directly to C pointers due to alignment issues
+! type(c_ptr),target :: c_sdx51D, c_sdy51D, c_sdx52D, c_sdy52D, c_sdx41D, c_sdy41D, &
+!                 c_sdx42D, c_sdy42D, c_rcx51D, c_rcy51D, c_rcx52D, c_rcy52D,&
+!                 c_rcx41D, c_rcy41D, c_rcx42D, c_rcy42D
+!
+! bind(C, name="sdx51D") :: c_sdx51D
+! bind(C, name="sdy51D") :: c_sdy51D
+! bind(C, name="sdx52D") :: c_sdx52D
+! bind(C, name="sdy52D") :: c_sdy52D
+! bind(C, name="sdx41D") :: c_sdx41D
+! bind(C, name="sdy41D") :: c_sdy41D
+! bind(C, name="sdx42D") :: c_sdx42D
+! bind(C, name="sdy42D") :: c_sdy42D
+! 
+! bind(C, name="rcx51D") :: c_rcx51D
+! bind(C, name="rcy51D") :: c_rcy51D
+! bind(C, name="rcx52D") :: c_rcx52D
+! bind(C, name="rcy52D") :: c_rcy52D
+! bind(C, name="rcx41D") :: c_rcx41D
+! bind(C, name="rcy41D") :: c_rcy41D
+! bind(C, name="rcx42D") :: c_rcx42D
+! bind(C, name="rcy42D") :: c_rcy42D
+ 
+! RATHER A WORKAROUND
+
+integer(c_int) :: c_sdx51_id = 80, & 
+            c_sdy51_id = 81 , & 
+            c_sdx52_id = 82 , &
+            c_sdy52_id = 83 , &
+            c_sdx41_id = 84 , &
+            c_sdy41_id = 85 , &
+            c_sdx42_id = 86 , &
+            c_sdy42_id = 87 , &
+            c_rcx51_id = 88 , &
+            c_rcy51_id = 89 , &
+            c_rcx52_id = 90 , &
+            c_rcy52_id = 91 , &
+            c_rcx41_id = 92 , &
+            c_rcy41_id = 93 , &
+            c_rcx42_id = 94 , &
+            c_rcy42_id = 95 
+
 END MODULE wave_field_comm
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 MODULE random_initial
@@ -373,6 +495,96 @@ MODULE random_initial
    return
   end subroutine assign_rand_ini
 END MODULE random_initial
+
+!-----------------------------------------------------------------------
+!Creates and initialises the Array_Info structure datatype
+!which stores meta-information related to each array.
+module metadata
+    use wave_field_comm
+    use itface_comm
+    use grid_node_comm
+    use iso_c_binding
+
+    IMPLICIT NONE
+
+    interface
+        subroutine init_gpuarr_metadata(nxtop, nytop, nztop, nxbtm, nybtm, nzbtm) &
+            bind(C, name="init_gpuarr_metadata")
+            use iso_c_binding
+            integer(c_int), intent(in), value :: nxtop, nytop, nztop, nxbtm, &
+                                                 nybtm, nzbtm
+        end subroutine init_gpuarr_metadata
+    end interface
+    TYPE :: Array_Info
+        integer :: num_dimensions
+        real(c_float), pointer, dimension(:):: arr_1d
+        real(c_float), pointer, dimension(:,:):: arr_2d
+        real(c_float), pointer, dimension(:,:,:):: arr_3d
+        integer(c_int) :: arr_id
+    end TYPE Array_Info
+    
+    TYPE(Array_Info) :: v1x_meta, v2x_meta, v1y_meta, v2y_meta, v1z_meta, v2z_meta, &
+                    t1xx_meta, t1xy_meta, t1xz_meta, t1yy_meta, t1yz_meta, t1zz_meta, &
+                    t2xx_meta, t2xy_meta, t2xz_meta, t2yy_meta, t2yz_meta, t2zz_meta, &
+                    sdx1_meta, sdx2_meta, sdy1_meta, sdy2_meta, &
+                    rcx1_meta, rcx2_meta, rcy1_meta, rcy2_meta, &
+                    sdx41_meta, sdx42_meta, sdx51_meta, sdx52_meta, & 
+                    sdy41_meta, sdy42_meta, sdy51_meta, sdy52_meta, & 
+                    rcx41_meta, rcx42_meta, rcx51_meta, rcx52_meta, & 
+                    rcy41_meta, rcy42_meta, rcy51_meta, rcy52_meta
+    
+    CONTAINS
+    
+    subroutine init_metadata () 
+        v1x_meta = Array_Info( 3, null(), null(), v1x, c_v1x_id) 
+        v1y_meta = Array_Info( 3, null(), null(), v1y, c_v1y_id) 
+        v1z_meta = Array_Info( 3, null(), null(), v1z, c_v1z_id)
+        v2x_meta = Array_Info( 3, null(), null(), v2x, c_v2x_id) 
+        v2y_meta = Array_Info( 3, null(), null(), v2y, c_v2y_id)
+        v2z_meta = Array_Info( 3, null(), null(), v2z, c_v2z_id) 
+
+        t1xx_meta = Array_Info( 3, null(), null()  , t1xx, c_t1xx_id)
+        t1xy_meta = Array_Info( 3, null(), null(), t1xy, c_t1xy_id)
+        t1xz_meta = Array_Info( 3, null(), null(), t1xz, c_t1xz_id)
+        t1yy_meta = Array_Info( 3, null(), null()  , t1yy, c_t1yy_id)
+        t1yz_meta = Array_Info( 3, null(), null(), t1yz, c_t1yz_id)
+        t1zz_meta = Array_Info( 3, null(), null()    , t1zz, c_t1zz_id)
+
+        t2xx_meta = Array_Info( 3, null(), null()  , t2xx, c_t2xx_id)
+        t2xy_meta = Array_Info( 3, null(), null(), t2xy, c_t2xy_id)
+        t2xz_meta = Array_Info( 3, null(), null(), t2xz, c_t2xz_id)
+        t2yy_meta = Array_Info( 3, null(), null()  , t2yy, c_t2yy_id)
+        t2yz_meta = Array_Info( 3, null(), null(), t2yz, c_t2yz_id)
+        t2zz_meta = Array_Info( 3, null(), null()  , t2zz, c_t2zz_id)
+
+        sdx1_meta = Array_Info( 1,  sdx1, null(), null(), c_sdx1_id) 
+        sdx2_meta = Array_Info( 1,  sdx2, null(), null(), c_sdx2_id) 
+        sdy1_meta = Array_Info( 1,  sdy1, null(), null(), c_sdy1_id) 
+        sdy2_meta = Array_Info( 1,  sdy2, null(), null(), c_sdy2_id) 
+        rcx1_meta = Array_Info( 1,  rcx1, null(), null(), c_rcx1_id) 
+        rcx2_meta = Array_Info( 1,  rcx2, null(), null(), c_rcx2_id) 
+        rcy1_meta = Array_Info( 1,  rcy1, null(), null(), c_rcy1_id) 
+        rcy2_meta = Array_Info( 1,  rcy2, null(), null(), c_rcy2_id) 
+
+        sdx41_meta = Array_Info( 3,  null(), null(), sdx41, c_sdx41_id)
+        sdx42_meta = Array_Info( 3,  null(), null(), sdx42, c_sdx42_id)
+        sdy41_meta = Array_Info( 3,  null(), null(), sdy41, c_sdy41_id)
+        sdy42_meta = Array_Info( 3,  null(), null(), sdy42, c_sdy42_id)
+        sdx51_meta = Array_Info( 3,  null(), null(), sdx51, c_sdx51_id)
+        sdx52_meta = Array_Info( 3,  null(), null(), sdx52, c_sdx52_id)
+        sdy51_meta = Array_Info( 3,  null(), null(), sdy51, c_sdy51_id)
+        sdy52_meta = Array_Info( 3,  null(), null(), sdy52, c_sdy52_id)
+
+        rcx41_meta = Array_Info( 3,  null(), null(), rcx41, c_rcx41_id)
+        rcx42_meta = Array_Info( 3,  null(), null(), rcx42, c_rcx42_id)
+        rcy41_meta = Array_Info( 3,  null(), null(), rcy41, c_rcy41_id)
+        rcy42_meta = Array_Info( 3,  null(), null(), rcy42, c_rcy42_id)
+        rcx51_meta = Array_Info( 3,  null(), null(), rcx51, c_rcx51_id)
+        rcx52_meta = Array_Info( 3,  null(), null(), rcx52, c_rcx52_id)
+        rcy51_meta = Array_Info( 3,  null(), null(), rcy51, c_rcy51_id)
+        rcy52_meta = Array_Info( 3,  null(), null(), rcy52, c_rcy52_id)
+    end subroutine init_metadata
+end module metadata
 
 ! 
 ! A timer which calls C gettimeofday function

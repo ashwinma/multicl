6b28737833ccbc3cf4fde5d5673625f5e05d6ac7
Ashwin Aji <ashwinma@gmail.com>
2015-01-27 21:17:29 +0000

    minor
    git-svn-id: svn+ssh://charon.cs.vt.edu/repos/svn/gpu_perfmod/branches/aaji@123 224d7eb1-a108-459d-91f0-3ca6aa5ee01a
    
    Former-commit-id: 08886fad6af2a1bfc085c22facf3ef5e4967bdd4
    Former-commit-id: 259ed7c92aa7b76babd1cbc315ad0436dc1c87dc

--- apps/sample-mq/foo.sh
+++ apps/sample-mq/foo.sh
@@ -1,12 +1,78 @@
-#for p in {0..2}
-#do
-#	for q in {0..2}
-#	do
-p=0
-q=0
-		for file in res.iter1024.float{*K,[124]M}.d${p}d${q}; do echo $file; awk '/Compute time for q\[1/ {print $(NF-3)}' $file; done
-		echo
-		for file in res.iter1024.float{*K,[124]M}.d${p}d${q}; do awk '/Marshal time for q\[0/ {print $(NF-3)}' $file; done
-		echo
-#	done
-#done
+for p in {1..2}
+do
+	for q in {1..2}
+	do
+		for file in res.iter1024.float4M.d${p}d${q}; do awk '/Compute time for Q\[1/ {print $(NF-3)}' $file; done
+	done
+done
+echo 
+for p in {1..2}
+do
+	for q in {1..2}
+	do
+		for file in res.iter1024.float4M.d${p}d${q}; do awk '/Marshal time for Q\[0/ {print $(NF-3)}' $file; done
+	done
+done
+echo
+for p in {1..2}
+do
+	for q in {1..2}
+	do
+		for file in res.iter1024.float2M.d${p}d${q}; do awk '/Compute time for Q\[1/ {print $(NF-3)}' $file; done
+	done
+done
+echo 
+for p in {1..2}
+do
+	for q in {1..2}
+	do
+		for file in res.iter1024.float2M.d${p}d${q}; do awk '/Marshal time for Q\[0/ {print $(NF-3)}' $file; done
+	done
+done
+for p in {1..2}
+do
+	for q in {1..2}
+	do
+		for file in res.iter1024.float1M.d${p}d${q}; do awk '/Compute time for Q\[1/ {print $(NF-3)}' $file; done
+	done
+done
+echo 
+for p in {1..2}
+do
+	for q in {1..2}
+	do
+		for file in res.iter1024.float1M.d${p}d${q}; do awk '/Marshal time for Q\[0/ {print $(NF-3)}' $file; done
+	done
+done
+echo
+for p in {1..2}
+do
+	for q in {1..2}
+	do
+		for file in res.iter1024.float512K.d${p}d${q}; do awk '/Compute time for Q\[1/ {print $(NF-3)}' $file; done
+	done
+done
+echo 
+for p in {1..2}
+do
+	for q in {1..2}
+	do
+		for file in res.iter1024.float512K.d${p}d${q}; do awk '/Marshal time for Q\[0/ {print $(NF-3)}' $file; done
+	done
+done
+echo
+for p in {1..2}
+do
+	for q in {1..2}
+	do
+		for file in res.iter1024.float256K.d${p}d${q}; do awk '/Compute time for Q\[1/ {print $(NF-3)}' $file; done
+	done
+done
+echo 
+for p in {1..2}
+do
+	for q in {1..2}
+	do
+		for file in res.iter1024.float256K.d${p}d${q}; do awk '/Marshal time for Q\[0/ {print $(NF-3)}' $file; done
+	done
+done

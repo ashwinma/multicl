1dd8946ea63cefc4054e9cfc4d1fcc73a979b54c
Ashwin Aji <aaji@cs.vt.edu>
2016-02-24 15:55:18 -0500

    moved snucl one level up
    Former-commit-id: a42895368186c853d3f0661085f5a8258752243a

--- snucl/apps/VT-DOE-FDM-Viz-OpenCL-OpenMP/fortranC.kaixi/mpiacc_wrappers.h
+++
@@ -1,6 +0,0 @@
-   subroutine MPI_Init_C (ierr)&
-        bind(C, name="MPI_Init_C")
-        use::iso_c_binding
-        integer(c_int), intent(inout):: ierr
-    end subroutine MPI_Init_C
-   
--- snucl/apps/VT-DOE-FDM-Viz-OpenCL-OpenMP/fortranOpenCL.kaixi/allocate_gpu_mem_opencl.h
+++
@@ -1,3 +0,0 @@
-call allocate_gpu_memC_opencl(lbx, lby, nmat, mw1_pml1, mw2_pml1, nxtop, &
-	nytop, nztop, mw1_pml, mw2_pml, nxbtm, nybtm, nzbtm, nzbm1, nll)
-

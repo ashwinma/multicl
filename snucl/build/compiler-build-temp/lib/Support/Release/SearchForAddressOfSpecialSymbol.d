5bc56bd86aa79d7fc2c674a3427a4be5307f99ba
Ashwin Aji <aaji@cs.vt.edu>
2016-02-24 05:18:20 -0500

    removed unnecessary files
    Former-commit-id: 093e3fdb83b84482d3388dac895f454ec2bd437e
    Former-commit-id: 0f80c34a76f79fc50a5d214fb50773528e81a0cb

--- snucl/build/compiler-build-temp/lib/Support/Release/SearchForAddressOfSpecialSymbol.d
+++
@@ -1,3 +0,0 @@
-/home/aaji/src/snucl/build/compiler-build-temp/lib/Support/Release/SearchForAddressOfSpecialSymbol.o \
- /home/aaji/src/snucl/build/compiler-build-temp/lib/Support/Release/SearchForAddressOfSpecialSymbol.d: \
- /home/aaji/src/snucl/src/compiler/lib/Support/SearchForAddressOfSpecialSymbol.cpp
--- snucl/build/compiler-build-temp/lib/Support/Release/SmallVector.d
+++
@@ -1,9 +0,0 @@
-/home/aaji/src/snucl/build/compiler-build-temp/lib/Support/Release/SmallVector.o \
- /home/aaji/src/snucl/build/compiler-build-temp/lib/Support/Release/SmallVector.d: \
- /home/aaji/src/snucl/src/compiler/lib/Support/SmallVector.cpp \
- /home/aaji/src/snucl/src/compiler/include/llvm/ADT/SmallVector.h \
- /home/aaji/src/snucl/src/compiler/include/llvm/Support/type_traits.h
-
-/home/aaji/src/snucl/src/compiler/include/llvm/ADT/SmallVector.h:
-
-/home/aaji/src/snucl/src/compiler/include/llvm/Support/type_traits.h:

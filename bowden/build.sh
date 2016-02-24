0b1b91180b0310e7dd80d5d0f50adece5c5b269e
Ashwin Aji <aaji@cs.vt.edu>
2016-02-24 05:15:00 -0500

    removed old files
    Former-commit-id: 0e66ea279948737539ae3532ae1922dbdeef244d

--- bowden/build.sh
+++
@@ -1,7 +0,0 @@
-nvcc -O3 -Xopencc="-LIST:source=on -O3" -arch=sm_21 -o gpuThrputBenchmark gpuThrputBenchmark.cu
-
-
-nvcc -O0 -Xopencc="-LIST:source=on -O0" -arch=sm_21 -o gpuThrputBenchmark gpuThrputBenchmark.cu
-
-
-nvcc -O0 -Xopencc="-LIST:source=on -O0" -arch=sm_20 -o gpuThrputBenchmark gpuThrputBenchmark.cu

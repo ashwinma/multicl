c0f05ae1b2423fb546d6662a872a6546e7ce0e1d
Ashwin Aji <aaji@cs.vt.edu>
2016-02-24 05:18:20 -0500

    removed unnecessary files
    Former-commit-id: 093e3fdb83b84482d3388dac895f454ec2bd437e
    Former-commit-id: 3dffc56f242eef0f912984a1151effc060e4b2d1

--- build/compiler-build-temp/lib/MC/Release/MCDisassembler.d
+++
@@ -1,9 +0,0 @@
-/home/aaji/src/snucl/build/compiler-build-temp/lib/MC/Release/MCDisassembler.o \
- /home/aaji/src/snucl/build/compiler-build-temp/lib/MC/Release/MCDisassembler.d: \
- /home/aaji/src/snucl/src/compiler/lib/MC/MCDisassembler.cpp \
- /home/aaji/src/snucl/src/compiler/include/llvm/MC/MCDisassembler.h \
- /home/aaji/src/snucl/build/compiler-build-temp/include/llvm/Support/DataTypes.h
-
-/home/aaji/src/snucl/src/compiler/include/llvm/MC/MCDisassembler.h:
-
-/home/aaji/src/snucl/build/compiler-build-temp/include/llvm/Support/DataTypes.h:
--- build/compiler-build-temp/runtime/libprofile/Release/OptimalEdgeProfiling.d
+++
@@ -1,9 +0,0 @@
-/home/aaji/src/snucl/build/compiler-build-temp/runtime/libprofile/Release/OptimalEdgeProfiling.o \
- /home/aaji/src/snucl/build/compiler-build-temp/runtime/libprofile/Release/OptimalEdgeProfiling.d: \
- /home/aaji/src/snucl/src/compiler/runtime/libprofile/OptimalEdgeProfiling.c \
- /home/aaji/src/snucl/src/compiler/runtime/libprofile/Profiling.h \
- /home/aaji/src/snucl/src/compiler/include/llvm/Analysis/ProfileInfoTypes.h
-
-/home/aaji/src/snucl/src/compiler/runtime/libprofile/Profiling.h:
-
-/home/aaji/src/snucl/src/compiler/include/llvm/Analysis/ProfileInfoTypes.h:

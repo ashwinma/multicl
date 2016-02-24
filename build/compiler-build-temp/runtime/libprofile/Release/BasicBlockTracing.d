3dffc56f242eef0f912984a1151effc060e4b2d1
Ashwin Aji <aaji@cs.vt.edu>
2016-02-24 05:18:20 -0500

    removed unnecessary files
    Former-commit-id: 093e3fdb83b84482d3388dac895f454ec2bd437e

--- build/compiler-build-temp/runtime/libprofile/Release/BasicBlockTracing.d
+++
@@ -1,9 +0,0 @@
-/home/aaji/src/snucl/build/compiler-build-temp/runtime/libprofile/Release/BasicBlockTracing.o \
- /home/aaji/src/snucl/build/compiler-build-temp/runtime/libprofile/Release/BasicBlockTracing.d: \
- /home/aaji/src/snucl/src/compiler/runtime/libprofile/BasicBlockTracing.c \
- /home/aaji/src/snucl/src/compiler/runtime/libprofile/Profiling.h \
- /home/aaji/src/snucl/src/compiler/include/llvm/Analysis/ProfileInfoTypes.h
-
-/home/aaji/src/snucl/src/compiler/runtime/libprofile/Profiling.h:
-
-/home/aaji/src/snucl/src/compiler/include/llvm/Analysis/ProfileInfoTypes.h:
--- build/compiler-build-temp/tools/clang/examples/snuclc-merger/Release/snuclc-merger.d
+++
@@ -1,3 +0,0 @@
-/home/aaji/src/snucl/build/compiler-build-temp/tools/clang/examples/snuclc-merger/Release/snuclc-merger.o \
- /home/aaji/src/snucl/build/compiler-build-temp/tools/clang/examples/snuclc-merger/Release/snuclc-merger.d: \
- /home/aaji/src/snucl/src/compiler/tools/clang/examples/snuclc-merger/snuclc-merger.cpp

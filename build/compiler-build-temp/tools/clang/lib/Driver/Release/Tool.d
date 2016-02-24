a65624cff02f076760b9c4d4995f0dab8ac6b934
Ashwin Aji <aaji@cs.vt.edu>
2016-02-24 05:18:20 -0500

    removed unnecessary files
    Former-commit-id: d2ff44173bc32694d7ac6c1f012a6471267542b5

--- build/compiler-build-temp/tools/clang/lib/Driver/Release/Tool.d
+++
@@ -1,6 +0,0 @@
-/home/aaji/src/snucl/build/compiler-build-temp/tools/clang/lib/Driver/Release/Tool.o \
- /home/aaji/src/snucl/build/compiler-build-temp/tools/clang/lib/Driver/Release/Tool.d: \
- /home/aaji/src/snucl/src/compiler/tools/clang/lib/Driver/Tool.cpp \
- /home/aaji/src/snucl/src/compiler/tools/clang/lib/Driver/../../include/clang/Driver/Tool.h
-
-/home/aaji/src/snucl/src/compiler/tools/clang/lib/Driver/../../include/clang/Driver/Tool.h:

b9924997a6867d072a34d29171899d4910786cc3
Ashwin Aji <aaji@cs.vt.edu>
2016-02-24 05:18:20 -0500

    removed unnecessary files
    Former-commit-id: 5675941f98dd164dc0184212485762b55a1da4d4

--- snucl/build/compiler-build-temp/runtime/libprofile/Release/EdgeProfiling.d
+++
@@ -1,9 +0,0 @@
-/home/aaji/src/snucl/build/compiler-build-temp/runtime/libprofile/Release/EdgeProfiling.o \
- /home/aaji/src/snucl/build/compiler-build-temp/runtime/libprofile/Release/EdgeProfiling.d: \
- /home/aaji/src/snucl/src/compiler/runtime/libprofile/EdgeProfiling.c \
- /home/aaji/src/snucl/src/compiler/runtime/libprofile/Profiling.h \
- /home/aaji/src/snucl/src/compiler/include/llvm/Analysis/ProfileInfoTypes.h
-
-/home/aaji/src/snucl/src/compiler/runtime/libprofile/Profiling.h:
-
-/home/aaji/src/snucl/src/compiler/include/llvm/Analysis/ProfileInfoTypes.h:
--- snucl/build/compiler-build-temp/utils/count/Release/count.d
+++
@@ -1,3 +0,0 @@
-/home/aaji/src/snucl/build/compiler-build-temp/utils/count/Release/count.o \
- /home/aaji/src/snucl/build/compiler-build-temp/utils/count/Release/count.d: \
- /home/aaji/src/snucl/src/compiler/utils/count/count.c
